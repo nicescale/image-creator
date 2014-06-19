81998721a34a256bd919b8c4191e6cd45f6ae29c
mountkin <mountkin@gmail.com>
2014-06-19 12:51:32 +0800

    auto determine terminal width

--- assets/bin/motd.rb
+++ assets/bin/motd.rb
@@ -13,14 +13,22 @@ def is_running(cid)
   @running_services.find { |x| cid.start_with?(x) }
 end
 
+term_cols = if test('x', '/usr/bin/tput')
+    `/usr/bin/tput cols`.to_i
+  elsif test('x', '/bin/stty')
+    `/bin/stty size`.split.last.to_i
+  else
+    120
+  end
+
 sids = FP::Vars.services_on_this_instance
 services = []
 sids.each { |sid|
   tags = FP::Vars.get_service_var(sid, 'deploy_tags', 'meta')
   next unless tags.kind_of?(Hash)
   spec = {
-    service_name: tags['service_name'],
-    service_version: tags['image_version'],
+    name: tags['service_name'],
+    service: tags['image_name'] + ':' + tags['image_version'],
     path: "/services/#{sid}",
     status: 'stopped',
     container_id: '',
@@ -34,10 +42,20 @@ sids.each { |sid|
     spec[:status] = 'running' if is_running(spec[:container_id])
   end
 
+  if term_cols.to_i < 80
+    spec.delete(:created_at)
+    spec.delete(:container_id)
+  elsif term_cols.to_i < 125
+    spec.delete(:created_at)
+  end
+
   services << spec
 }
 
 Formatador.display_line "[green]Welcome to use NiceScale."
 Formatador.display_line "You have the folling services running on this server:[/]"
 puts ""
-Formatador.display_table services
+Formatador.display_table(services) do
+  # Disable sorting headers.
+  0
+end
