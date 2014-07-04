1173cdde5809974d397832b6d58b951f32ec597d
mountkin <mountkin@gmail.com>
2014-07-04 14:49:18 +0800

    fix url\

--- assets/bin/first-boot.sh
+++ assets/bin/first-boot.sh
@@ -116,7 +116,7 @@ function cleanup {
 function load_credentials {
   [ -d $init_conf_dir ] || mkdir -p $init_conf_dir
   for i in `seq 1 120`; do
-    local url="$cpi_base_url/internal/instance-credentials/`get_instance_id`/`sign`.text"
+    local url="$cpi_base_url/internal/instance-credentials/`get_instance_id`/`sign`"
     local http_status=`curl -s -o ${init_conf_path} -w '%{http_code}' $url`
     [ $http_status = 200 ] && break
     sleep 1
